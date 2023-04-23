f6333e94e5e7caa11ffa513ca965e0b623ae50d5
Travis Quinnelly <tquinnelly@gmail.com>
2023-04-22 19:26:59 -0500

    Update scan.sh
    Removed echos

--- scan.sh
+++ scan.sh
@@ -1,35 +1,20 @@
 #!/bin/bash
-
 set -euo pipefail
-echo ""
-echo "$( date -I'seconds' ) ClamAV process starting"
-echo ""
-echo "Updating ClamAV scan DB"
+echo "\n$( date -I'seconds' ) ClamAV process starting\n"
+echo "\Unpdating ClamAV scan DB\n"
 set +e
 freshclam
 FRESHCLAM_EXIT=$?
 set -e
-echo ""
-
 if [[ "$FRESHCLAM_EXIT" -eq "0" ]]; then
-    echo ""
-    echo "Freshclam updated the DB"
-    echo ""
+    echo "\nFreshclam updated the DB\n"
 elif [[ "$FRESHCLAM_EXIT" -eq "1" ]]; then
-    echo ""
-    echo "ClamAV DB already up to date..."
-    echo ""
+    echo "\nClamAV DB already up to date...\n"
 else
-    echo ""
-    echo "An error occurred (freshclam returned with exit code '$FRESHCLAM_EXIT')"
-    echo ""
+    echo "\nAn error occurred (freshclam returned with exit code '$FRESHCLAM_EXIT')\n"
     exit $FRESHCLAM_EXIT
 fi
-echo ""
 clamscan -V
-echo ""
-echo "Scanning $SCANDIR"
-echo ""
+echo "\nScanning $SCANDIR"
 clamscan -r $SCANDIR $@
-echo ""
 echo "$( date -I'seconds' ) ClamAV scanning finished"
