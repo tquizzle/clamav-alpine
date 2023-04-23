28c01c7c30d18cbed26974d1bbd029ba9825762e
Travis Quinnelly <tquinnelly@gmail.com>
2023-04-23 06:56:27 -0500

    fixed

--- scan.sh
+++ scan.sh
@@ -7,14 +7,14 @@ freshclam
 FRESHCLAM_EXIT=$?
 set -e
 if [[ "$FRESHCLAM_EXIT" -eq "0" ]]; then
-    echo -e "Freshclam updated the DB \n"
+    echo -e "\nFreshclam updated the DB \n"
 elif [[ "$FRESHCLAM_EXIT" -eq "1" ]]; then
-    echo -e "ClamAV DB already up to date... \n"
+    echo -e "\nClamAV DB already up to date... \n"
 else
-    echo -e "An error occurred (freshclam returned with exit code '$FRESHCLAM_EXIT') \n"
+    echo -e "\nAn error occurred (freshclam returned with exit code '$FRESHCLAM_EXIT') \n"
     exit $FRESHCLAM_EXIT
 fi
 clamscan -V
-echo -e "Scanning $SCANDIR \n"
+echo -e "\nScanning $SCANDIR \n"
 clamscan -r $SCANDIR $@
-echo -e "$( date -I'seconds' ) ClamAV scanning finished \n"
+echo -e "\n $( date -I'seconds' ) ClamAV scanning finished \n"
