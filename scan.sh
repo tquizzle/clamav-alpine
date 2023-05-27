c6f2f715605ac57074517c6985dd5eb55cff020d
Travis Quinnelly <tquinnelly@gmail.com>
2023-05-27 07:04:01 -0500

    bring the echos back

--- scan.sh
+++ scan.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
 set -euo pipefail
-echo -e "$( date -I'seconds' ) ClamAV process starting \n"
-echo -e "Unpdating ClamAV scan DB \n"
+echo ""
+echo -e "$( date -I'seconds' ) ClamAV process starting"
+echo ""
+echo -e "Unpdating ClamAV scan DB"
+echo ""
 set +e
 freshclam
 FRESHCLAM_EXIT=$?
 set -e
 if [[ "$FRESHCLAM_EXIT" -eq "0" ]]; then
-    echo -e "\nFreshclam updated the DB \n"
+    echo ""
+    echo -e "Freshclam updated the DB"
+    echo ""
 elif [[ "$FRESHCLAM_EXIT" -eq "1" ]]; then
-    echo -e "\nClamAV DB already up to date... \n"
+    echo ""
+    echo -e "ClamAV DB already up to date..."
+    echo ""
 else
-    echo -e "\nAn error occurred (freshclam returned with exit code '$FRESHCLAM_EXIT') \n"
+    echo ""
+    echo -e "An error occurred (freshclam returned with exit code '$FRESHCLAM_EXIT')"
+    echo ""
     exit $FRESHCLAM_EXIT
 fi
 clamscan -V
-echo -e "\nScanning $SCANDIR \n"
+echo ""
+echo -e "Scanning $SCANDIR"
+echo ""
 clamscan -r $SCANDIR $@
-echo -e "\n $( date -I'seconds' ) ClamAV scanning finished \n"
+echo ""
+echo -e "$( date -I'seconds' ) ClamAV scanning finished"
